3395038cb84cedfe73a3176708a71ad6b313e545
chap0322 <132544510+chap0322@users.noreply.github.com>
2023-05-04 01:46:25 -0500

    Update and rename crear.php to database.php

--- crear.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    //Base de datos
-    require '../../includes/config/database.php';
-    $db=conectarDB();
-
-    // Consultar para obtener los vendedores
-    $consulta = "SELECT * FROM vendedores";
-    $resultado = mysqli_query($db, $consulta);
